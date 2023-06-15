045e80fb06b6184733ae905e6e4b1891a6e78de0
Softy Plug <132814762+softy-plug@users.noreply.github.com>
2023-06-15 13:43:33 +0300

    text_to_console
    text_to_console - simple program for show any text on Windows or Mac/Unix/Linux console

---
+++ text_to_console/readme.txt
@@ -0,0 +1,8 @@
+╭━━╮╱╭━┳╮╱╱╱╭━╮╱╱╱╱╱╱
+┃━━╋━┫━┫╰┳┳╮┃╋┣╮╭┳┳━╮
+┣━━┃╋┃╭┫╭┫┃┃┃╭┫╰┫┃┃╋┃
+╰━━┻━┻╯╰━╋╮┃╰╯╰━┻━╋╮┃
+╱╱╱╱╱╱╱╱╱╰━╯╱╱╱╱╱╱╰━╯
+softy_plug
+
+text_to_console - simple program for show any text on Windows or Mac/Unix/Linux console
---
+++ text_to_console/text_to_console.py
@@ -0,0 +1,25 @@
+import os
+import time
+ 
+os.system('cls' if os.name == 'nt' else 'clear') # clear the terminal window for Windows or Unix/Linux/MacOS
+ 
+print("╭━━╮╱╭━┳╮╱╱╱╭━╮╱╱╱╱╱╱")
+print("┃━━┋━┫━┫╰┳┳╮┃╋┣╮╭┳┳━╮")
+print("┣━━┃╋┃╭┫╭┫┃┃┃╭┫╰┫┃┃╋┃")
+print("╰━━┻━┻╯╰━╋╮┃╰╯╰━┻━╋╮┃")
+print("╱╱╱╱╱╱╱╱╱╰━╯╱╱╱╱╱╱╰━╯")
+print("softy_plug")
+
+time.sleep(1) # wait for 10 seconds before quitting the program
+
+# wait for the user to type some text 
+user_input = input("Type in some text to display on console:")
+
+# display the user's text
+print(user_input)
+ 
+time.sleep(10) # wait for 10 seconds before quitting the program
+
+os.system('cls' if os.name == 'nt' else 'clear') # clear the terminal window for Windows or Unix/Linux/MacOS
+time.sleep(1) # wait for 10 seconds before quitting the program 
+#softy_plug
